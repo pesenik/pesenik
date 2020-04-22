0c61cc6cb47d6dae9055257ada648f8295ca85e7
nmorozov <nmorozov@kraftway.ru>
2020-04-22 20:48:17 +0300

    Sanic to Flask changed

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,6 @@
-import os
+from flask import Flask
+app = Flask(__name__)
 
-from sanic import Sanic
-
-app = Sanic()
-
-PORT = int(os.env.get("PORT", "8000"))
-
-@app.route("/")
-def index():
+@app.route('/')
+def hello_world():
     return 'Hello, World!'
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=PORT)
