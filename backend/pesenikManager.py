b690b2a84e8d2f47fe807961b92975bd00b5040a
Vladimir Liutov <vs@lutov.net>
2020-04-24 18:54:31 +0000

    code style

--- backend/pesenikManager.py
+++ backend/pesenikManager.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class Song:
     title: str
@@ -7,6 +8,7 @@ class Song:
     authorHash: str
     text: str
     songTitleHash: str
+
 
 class PesenikManager:
     def authorsList(self):
@@ -19,19 +21,34 @@ class PesenikManager:
             return ["aab000"]
         return []
 
-    def getSong (self, author, song):
+    def getSong(self, author, song):
         if author == "aaa":
-            return Song("Group1Song1", "Group1", author,   """"Group1Song1Text:
-                    А я ёжиков люблю""" , song)
+            return Song(
+                "Group1Song1",
+                "Group1",
+                author,
+                """"Group1Song1Text:
+                    А я ёжиков люблю""",
+                song,
+            )
         elif author == "aab":
-            return  Song("Group2Song1", "Group2", author,   """"Group2Song1Text:
-                    В каморке, что за актовым залом...""" , song)
+            return Song(
+                "Group2Song1",
+                "Group2",
+                author,
+                """"Group2Song1Text:
+                    В каморке, что за актовым залом...""",
+                song,
+            )
 
     def authorToHash(self, author):
         return "aaa" if author == "Group1" else "aab"
+
     def hashToAutor(self, authorHash):
         return "Group1" if authorHash == "aaa" else "Group2"
+
     def songTitleToHash(self, songTitle):
         return "aaa000" if songTitle == "Group1Song1" else "aab000"
+
     def hashToSongTitle(self, songTitleHash):
         return "Group1Song1" if songTitleHash == "aaa" else "Group2Song1"
